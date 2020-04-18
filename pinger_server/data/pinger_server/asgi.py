4be3499dd2a689aca37c18ca5af2060022f6cb4c
blender <blender@pop-os.localdomain>
2020-04-18 23:11:36 +1000

    total restructure

--- pinger_server/data/pinger_server/asgi.py
+++ pinger_server/data/pinger_server/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for pinger project.
+ASGI config for pinger_server project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pinger.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pinger_server.settings')
 
 application = get_asgi_application()
