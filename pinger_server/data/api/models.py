76eda579c1611ce4266f5b15a8a2bc56fe0b17f2
blender <blender@pop-os.localdomain>
2020-05-10 07:40:29 +1000

    updates

--- pinger_server/data/api/models.py
+++ pinger_server/data/api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils import timezone
 
 # Create your models here.
+
+class UserTargets(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    target_name = models.CharField(max_length=100)
+    target_address = models.CharField(null=True, max_length = 255)
+    icmp_size = models.IntegerField(null=True, default = 500, validators=[MinValueValidator(100), MaxValueValidator(1000)])
+    icmp_interval = models.IntegerField(null=True, default=1, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    icmp_count = models.IntegerField(null=True, default=5, validators=[MinValueValidator(5), MaxValueValidator(10)]) 
+
+    class Meta:
+        verbose_name = "User Target"
+        verbose_name_plural = "User Targets"
+        unique_together = (('user', 'target_name'),)
+        unique_together = (('user', 'target_address'),)
+        index_together = (('user', 'target_name'),)
+        index_together = (('user', 'target_address'),)
+
+    def __str__(self):
+        return str(self.user) + " - " + self.target_name + " - " + self.target_address
--- pinger_server/data/api/serializers.py
+++ pinger_server/data/api/serializers.py
@@ -1,20 +1,27 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from collector.models import icmp_results, targets
+from .models import UserTargets
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
-        #fields = '__all__'
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
 
 class ProfileSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
-    
-        fields = ['username']
+        fields = ['id', 'username']
+
+class UserTargetSerializer(serializers.ModelSerializer):
+
+    def to_representation(self, instance):
+        self.fields['user'] =  UserSerializer(read_only=True)
+        return super(UserTargetSerializer, self).to_representation(instance)
+
+    class Meta:
+        model = UserTargets
+        fields = ['id', 'user_id', 'target_name', 'target_address', 'icmp_size', 'icmp_interval', 'icmp_count', 'user']
+
